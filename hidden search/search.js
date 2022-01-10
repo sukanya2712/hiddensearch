8bdd5aa1122021190e2680902c9f20447fab0dd3
sukanya2712 <95672385+sukanya2712@users.noreply.github.com>
2022-01-10 17:09:56 +0530

    Delete hidden search directory

--- hidden search/search.js
+++
@@ -1,8 +0,0 @@
-const searchxyz= document.querySelector(".search")
-const btnxyz= document.querySelector(".btn")
-const inputxyz= document.querySelector(".input")
-
-btnxyz.addEventListener("click",function(){
-    searchxyz.classList.toggle("active")
-    inputxyz.focus()
-})
